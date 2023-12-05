fc3fc96ba3dc43b6e637f2d10a2b2bd4de55af67
CarryMiss <1023638599@qq.com>
2023-12-05 15:26:13 +0800

    happyLemon 类型转换 00021

--- src/go_code/project01/main/variable02.go
+++ src/go_code/project01/main/variable02.go
@@ -11,4 +11,15 @@ func main() {
 	var num2 float32 = float32(num1)
 	fmt.Println("num2=", num2)
 
+	// int -> string
+	var num3 int32 = 99
+	var num4 string = fmt.Sprintf("%d", num3)
+	fmt.Println("num4=", num4)
+
+	// string -> int
+	var str string = "123"
+	var num5 int
+	fmt.Sscanf(str, "%d", &num5)
+	fmt.Println("num5=", num5)
+
 }
