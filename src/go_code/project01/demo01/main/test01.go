0b51fc8b4c746c996f91d4940cbb2a8f0bcadf5f
CarryMiss <1023638599@qq.com>
2023-12-05 22:16:28 +0800

    happyLemon 练习 00034

--- src/go_code/project01/demo01/main/test01.go
+++ src/go_code/project01/demo01/main/test01.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	test01()
+	test02()
 }
 
 func test01() {
@@ -12,4 +13,11 @@ func test01() {
 	var week int = days / 7
 	var day int = days % 7
 	fmt.Println(week, "个星期零", day, "天")
+}
+
+func test02() {
+	// 定义一个变量保存华氏温度，华氏温度转换摄氏温度的公式为：5/9*(华氏温度-100)，请求出华氏温度对应的摄氏温度
+	var huashi float32 = 134.5
+	var sheshi float32 = 5.0 / 9 * (huashi - 100)
+	fmt.Printf("华氏温度 %v 对应的摄氏温度为 %v \n", huashi, sheshi)
 }
