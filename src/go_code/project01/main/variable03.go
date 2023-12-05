ee9343b50ab6f8a9a5d569c85e8fc28b73f76c2e
CarryMiss <1023638599@qq.com>
2023-12-05 18:47:14 +0800

    happyLemon 指针 00028

--- src/go_code/project01/main/variable03.go
+++ src/go_code/project01/main/variable03.go
@@ -11,4 +11,10 @@ func main() {
 	// 创建一个指针变量
 	var ptr *int = &i
 	fmt.Printf("ptr的类型是%T，ptr的值是%v，ptr的地址是%v \n", ptr, ptr, &ptr)
+
+	// 通过指针改变原数据
+	var num1 int = 9
+	var ptr1 *int = &num1
+	*ptr1 = 10 // *ptr1代表取出ptr1指针指向的值
+	fmt.Println("num1=", num1)
 }
