72f9d8f7f6c2012c154f76daff5f67e233e5608a
CarryMiss <1023638599@qq.com>
2023-12-05 16:51:34 +0800

    happyLemon 类型转换 00023

--- src/go_code/project01/main/variable02.go
+++ src/go_code/project01/main/variable02.go
@@ -19,11 +19,19 @@ func main() {
 	// 第一个参数是要转换的值，第二个参数是转换的进制
 	num6 := strconv.FormatInt(int64(num3), 10)
 	fmt.Println("num6=", num6)
+	num7 := strconv.Itoa(int(num3))
+	fmt.Println("num7=", num7)
 
 	// string -> int
 	var str string = "123"
 	var num5 int
 	fmt.Sscanf(str, "%d", &num5)
 	fmt.Println("num5=", num5)
+
+	// string -> bool
+	var str2 string = "true"
+	var b bool
+	b, _ = strconv.ParseBool(str2) //这里的_是一个占位符，用于忽略函数返回的错误值。如果str2不能被解析为一个布尔值，ParseBool函数会返回一个错误，但在这里，我们选择忽略这个错误。
+	fmt.Println("b=", b)
 
 }
