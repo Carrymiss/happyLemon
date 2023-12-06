90cf544a92cd751dd18801fc89647e1bc2723bd6
CarryMiss <1023638599@qq.com>
2023-12-06 13:41:38 +0800

    happyLemon switch 00046

--- src/go_code/project01/main/aaab_proControl01.go
+++ src/go_code/project01/main/aaab_proControl01.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	test03()
+}
+func test03() {
+	// switch分支
+	var num int
+	fmt.Println("请输入一个数字")
+	fmt.Scanln(&num)
+	switch num {
+	case 1:
+		fmt.Println("你输入的数字是1")
+	case 2:
+		fmt.Println("你输入的数字是2")
+	case 3:
+		fmt.Println("你输入的数字是3")
+	default:
+		fmt.Println("你输入的数字是其他数字")
+	}
 }
