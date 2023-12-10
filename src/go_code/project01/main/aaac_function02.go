b3b5f9cf044f471de4f7ff4f840afa235d64d3bc
CarryMiss <1023638599@qq.com>
2023-12-10 12:13:40 +0800

    happyLemon init函数 00089

--- src/go_code/project01/main/aaac_function02.go
+++ src/go_code/project01/main/aaac_function02.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"happyLemon/src/go_code/project01/demo01"
+)
 
 // 定义全局变量
 var age = test19()
@@ -12,6 +15,8 @@ func main() {
 
 // init函数
 func init() {
+	demo01.Age = 100
+	demo01.Name = "jack"
 	fmt.Println("init函数执行了")
 }
 
@@ -24,6 +29,8 @@ func test19() int {
 func test18() {
 	fmt.Println("main函数执行了")
 	fmt.Println("age=", age)
+	fmt.Println("demo01.Age=", demo01.Age)
+	fmt.Println("demo01.Name=", demo01.Name)
 	// 分割线
 	fmt.Println("-------------分割线--------------")
 }
