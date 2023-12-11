b7b623527d80f2918ff55e6e0c4ffc01096e9910
CarryMiss <1023638599@qq.com>
2023-12-11 20:58:35 +0800

    happyLemon 练习 00140

--- src/go_code/project01/demo01/main/test02.go
+++ src/go_code/project01/demo01/main/test02.go
@@ -217,20 +217,17 @@ func test30() {
 
 func test31() {
 	// 输出100以内的所有素数，每行显示5个，并求和
-	var sum int = 0
 	var count int = 0
 	for i := 2; i <= 100; i++ {
 		flag := true
 		for j := 2; j <= i/2; j++ {
 			if i%j == 0 {
 				flag = false
-				break
 			}
 		}
 		if flag {
 			fmt.Print(i, "\t")
 			count++
-			sum += i
 			if count%5 == 0 {
 				fmt.Println()
 			}
