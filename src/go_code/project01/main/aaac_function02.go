f7715e6abce537a18e6e674e19b6afe6ebd6cf2f
CarryMiss <1023638599@qq.com>
2023-12-11 14:26:30 +0800

    happyLemon 时间日期常用函数 00124

--- src/go_code/project01/main/aaac_function02.go
+++ src/go_code/project01/main/aaac_function02.go
@@ -315,6 +315,7 @@ func test30() {
 	// 获取年月日时分秒
 	fmt.Printf("年=%v \n", now.Year())
 	fmt.Printf("月=%v \n", now.Month())
+	fmt.Printf("月=%v \n", int(now.Month()))
 	fmt.Printf("日=%v \n", now.Day())
 	fmt.Printf("时=%v \n", now.Hour())
 	fmt.Printf("分=%v \n", now.Minute())
