ee9ac4b9ca1293404270705895ff2b5a2b03a0af
CarryMiss <1023638599@qq.com>
2023-12-13 22:12:15 +0800

    happyLemon map使用 00198

--- src/go_code/project01/main/aaac_variable04.go
+++ src/go_code/project01/main/aaac_variable04.go
@@ -1,9 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	// map
+	test61()
 }
 
 func test61() {
-
+	// 声明map
+	var a map[string]string
+	// 在使用map前，需要先make，make的作用就是给map分配数据空间
+	a = make(map[string]string, 10)
+	a["no1"] = "宋江"
+	a["no2"] = "吴用"
+	a["no1"] = "武松"
+	fmt.Print("a的值是：", a)
+	// 分割线
+	println("-------------分割线--------------")
 }
