fb579576c7e5c901d94829e81fbef3994f7a95f8
CarryMiss <1023638599@qq.com>
2023-12-13 19:53:31 +0800

    happyLemon 练习 00185

--- src/go_code/project01/demo01/main/test03.go
+++ src/go_code/project01/demo01/main/test03.go
@@ -12,6 +12,9 @@ func main() {
 
 	// 将斐波那契数列的前50个数放到切片中
 	test42(50)
+
+	// 定义一个二维数组用来保存三个班，每个班五名同学的成绩，并求出每个班的平均分和所有班级的平均分
+	test43()
 }
 
 func test41() {
@@ -42,6 +45,27 @@ func test42(num int) {
 		slice1[i] = slice1[i-1] + slice1[i-2]
 	}
 	fmt.Println("此时生成的切片是：", slice1)
+	// 分割线
+	println("-------------分割线--------------")
+}
+
+func test43() {
+	// 定义一个二维数组用来保存三个班，每个班五名同学的成绩，并求出每个班的平均分和所有班级的平均分
+	var arr = [3][5]float64{{18, 29, 73, 45, 78}, {17, 24, 34, 46, 65}, {14, 52, 63, 44, 20}}
+	// 每个班的成绩和
+	var arr2 [3]float64
+	// 每个班级的平均分
+	var arr3 [3]float64
+	for index1, value1 := range arr {
+		var sum float64 = 0
+		for _, value2 := range value1 {
+			sum += value2
+		}
+		arr2[index1] = sum
+		arr3[index1] = sum / float64(len(value1))
+	}
+	fmt.Println("每个班的成绩和是：", arr2)
+	fmt.Println("每个班的平均分是：", arr3)
 	// 分割线
 	println("-------------分割线--------------")
 }
