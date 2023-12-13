5efd4ca33877e13d744c723ddfd13c79a0c5f6a7
CarryMiss <1023638599@qq.com>
2023-12-13 08:57:36 +0800

    happyLemon 二维数组 00181

--- src/go_code/project01/main/aaac_variable03.go
+++ src/go_code/project01/main/aaac_variable03.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// 二维数组
 	test58()
@@ -18,6 +20,15 @@ func test58() {
 		}
 		println()
 	}
+	// 分割线
+	println("-------------分割线--------------")
+
+	// 正好差了48字节 也就是6个int64 因为上面定义的这个数组二维的元素有6个
+	fmt.Printf("arr[0]的地址是：%p \n", &arr[0])
+	fmt.Printf("arr[1]的地址是：%p \n", &arr[1])
+	fmt.Printf("arr[1][0]的地址是：%p \n", &arr[1][0])
+	fmt.Printf("arr[1][1]的地址是：%p \n", &arr[1][1])
+
 	// 分割线
 	println("-------------分割线--------------")
 }
