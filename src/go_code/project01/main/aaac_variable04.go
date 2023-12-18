887e465f4f6104877f9cd79ea4680a4f0856b6ac
CarryMiss <1023638599@qq.com>
2023-12-18 20:38:31 +0800

    happyLemon map使用 00203

--- src/go_code/project01/main/aaac_variable04.go
+++ src/go_code/project01/main/aaac_variable04.go
@@ -7,6 +7,7 @@ func main() {
 	test61()
 	test62()
 	test63()
+	test64()
 }
 
 func test61() {
@@ -80,4 +81,40 @@ func test63() {
 	} else {
 		fmt.Println("没有找到no3")
 	}
+}
+
+func test64() {
+	// map是引用类型
+	// map 回自动扩容
+	map1 := make(map[int]int, 2)
+	map1[1] = 90
+	map1[2] = 80
+	map1[3] = 70
+	map1[4] = 60
+	test65(map1)
+	fmt.Println("map1的值是：", map1)
+	// 分割线
+	println("-------------分割线--------------")
+}
+
+func test65(map1 map[int]int) {
+	map1[1] = 100
+}
+
+type stu struct {
+	Name    string
+	Age     int
+	Address string
+}
+
+func test66() {
+	students := make(map[string]stu, 10)
+	// 创建两个学生
+	stu1 := stu{"tom", 18, "北京"}
+	stu2 := stu{"mary", 28, "上海"}
+	students["no1"] = stu1
+	students["no2"] = stu2
+	fmt.Println("students的值是：", students)
+	// 分割线
+	println("-------------分割线--------------")
 }
