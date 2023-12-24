8b40c30a13d5328dffb253058c32629735b7bda4
CarryMiss <1023638599@qq.com>
2023-12-24 11:07:14 +0800

    happyLemon 工厂模式 00227

--- src/go_code/project01/main/aaad_variable01.go
+++ src/go_code/project01/main/aaad_variable01.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"happyLemon/src/go_code/project01/demo01"
+)
 
 func main() {
 	// 定义结构体
 	test83()
+
+	// 工厂模式
+	test84()
 }
 
 type stundent1 struct {
@@ -18,6 +24,14 @@ func test83() {
 	// 之前定义的时候需要遵循顺序，这样就不用遵循顺序了
 	var stu1 = stundent1{Name: "tom", Age: 18, Adress: "北京"}
 	fmt.Println(stu1)
+	// 分割线
+	println("-------------分割线--------------")
+}
+
+func test84() {
+	person := demo01.NewPerson("tom", 18)
+	fmt.Println(*person)
+	fmt.Println("name =", person.Name, "age =", person.Age)
 	// 分割线
 	println("-------------分割线--------------")
 }
