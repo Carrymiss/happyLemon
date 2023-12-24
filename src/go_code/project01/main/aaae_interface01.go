388bee360c9764b7dce9bf3b91d608215a30188d
CarryMiss <1023638599@qq.com>
2023-12-24 19:24:29 +0800

    happyLemon 接口 00234

--- src/go_code/project01/main/aaae_interface01.go
+++ src/go_code/project01/main/aaae_interface01.go
@@ -2,6 +2,7 @@ package main
 
 func main() {
 	// 接口
+	test87()
 }
 
 type Usb interface {
@@ -32,4 +33,21 @@ func (c Camera) Start() {
 
 func (c Camera) Stop() {
 	println("相机停止工作")
+}
+
+type Computer struct {
+}
+
+// Working 只要实现了usb接口（所谓实现usb接口，就是指实现了usb接口声明所有方法）
+func (c Computer) Working(usb Usb) {
+	usb.Start()
+	usb.Stop()
+}
+
+func test87() {
+	var c Computer
+	c.Working(Phone{"vivo"})
+	c.Working(Camera{"尼康"})
+	// 分割线
+	println("-------------分割线--------------")
 }
