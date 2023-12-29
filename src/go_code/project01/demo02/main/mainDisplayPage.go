0f5e905c15eb52bf5221d3393648f94f300aa82b
CarryMiss <1023638599@qq.com>
2023-12-29 20:23:36 +0800

    happyLemon 家庭收支记账软件 00243

--- src/go_code/project01/demo02/main/mainDisplayPage.go
+++ src/go_code/project01/demo02/main/mainDisplayPage.go
@@ -11,6 +11,19 @@ func displayPage() {
 	key := ""
 	// 声明一个变量,控制是否退出for
 	loop := true
+
+	// 定义账户的余额
+	balance := 10000.0
+	// 每次收支的金额
+	money := 0.0
+	// 每次收支的说明
+	note := ""
+	// 定义一个变量,记录是否有收支的行为
+	//flag := false
+	// 收支的详情使用字符串来记录
+	// 当有收支时,只需要对details进行拼接处理即可
+	details := "收支\t账户金额\t收支金额\t说    明"
+
 	// 显示主菜单
 	for {
 		fmt.Println("----------------家庭收支记账软件----------------")
@@ -22,9 +35,18 @@ func displayPage() {
 		fmt.Scanln(&key)
 		switch key {
 		case "1":
-			fmt.Println("收支明细")
+			fmt.Println("----------------当前收支明细记录----------------")
+			fmt.Println(details)
 		case "2":
-			fmt.Println("登记收入")
+			fmt.Println("本次收入金额:")
+			fmt.Scanln(&money)
+			balance += money // 修改账户余额
+			fmt.Println("本次收入说明:")
+			fmt.Scanln(&note)
+			// 将本次收入情况,拼接到details变量
+			// 收入    11000    1000    有人发红包
+			//details += fmt.Sprintf("\n收入\t%v\t%v\t%v", balance, money, note)
+			details += fmt.Sprintf("\n收入\t%v\t%v\t%v", balance, money, note)
 		case "3":
 			fmt.Println("登记支出")
 		case "4":
