baac315cc9da35d68c479511ba31eeabbbf43cb0
CarryMiss <1023638599@qq.com>
2024-01-01 13:19:22 +0800

    happyLemon 单元测试 00287

--- src/go_code/project01/main/aaag_unitTest01.go
+++ src/go_code/project01/main/aaag_unitTest01.go
@@ -1,9 +1,23 @@
 package main
 
-func main() {
+import "testing"
 
+// 被测试函数
+func test104(n int) int {
+	res := 1
+	for i := 1; i <= n; i++ {
+		res += i
+	}
+	return res
 }
 
-func test104() {
-
+func Test1(t *testing.T) {
+	res := test104(10)
+	if res != 55 {
+		t.Fatalf("test104(10) error, expected:%v, actual:%v", 55, res)
+	}
+	// 如果正确，输出日志
+	t.Logf("test104(10) success, expected:%v, actual:%v", 55, res)
+	// 分割线
+	println("-------------分割线--------------")
 }
