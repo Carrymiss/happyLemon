2407d22a16bc4161d4748775fce302bfac80ada8
CarryMiss <1023638599@qq.com>
2024-01-01 14:24:01 +0800

    happyLemon 单元测试 00290

---
+++ src/go_code/project01/test/aaag_unitTest02_test.go
@@ -0,0 +1,22 @@
+package test
+
+import (
+	"happyLemon/src/go_code/project01/demo01/domain"
+	"testing"
+)
+
+func Test2(t *testing.T) {
+	monster := domain.Monster{
+		Name:  "牛魔王",
+		Age:   500,
+		Skill: "牛魔拳",
+	}
+	res := monster.Store()
+	if !res {
+		t.Fatalf("monster.Store() error , expected:%v, actual:%v", true, res)
+	}
+	t.Logf("monster.Store() success, expected:%v, actual:%v", true, res)
+
+	// 分割线
+	println("-------------分割线--------------")
+}
