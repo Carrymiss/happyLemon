3b184313a920748327db0c539bc1983d09685f44
CarryMiss <1023638599@qq.com>
2024-01-01 12:44:22 +0800

    happyLemon 反序列化 00284

--- src/go_code/project01/main/aaag_seri02.go
+++ src/go_code/project01/main/aaag_seri02.go
@@ -5,6 +5,8 @@ import "encoding/json"
 func main() {
 	// 将json字符串，反序列化成struct
 	test101()
+	// 将json字符串，反序列化成map
+	test102()
 }
 func test101() {
 	// 将json字符串，反序列化成struct
@@ -24,4 +26,19 @@ type Person1 struct {
 	Name    string `json:"name"`
 	Age     int    `json:"age"`
 	Address string `json:"address"`
+}
+
+func test102() {
+	// 将json字符串，反序列化成map
+	str := `{"age":18,"name":"jack","address":"北京"}`
+	// 这里不需要make map，因为Unmarshal函数内部会make
+	var m map[string]interface{}
+	// 反序列化
+	err := json.Unmarshal([]byte(str), &m)
+	if err != nil {
+		println("反序列化失败")
+	}
+	println(m)
+	// 分割线
+	println("-------------分割线--------------")
 }
