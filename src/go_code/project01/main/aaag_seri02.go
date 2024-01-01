d5c5aebce58aa643454be9b02318fc07ca412452
CarryMiss <1023638599@qq.com>
2024-01-01 12:39:46 +0800

    happyLemon 反序列化 00283

---
+++ src/go_code/project01/main/aaag_seri02.go
@@ -0,0 +1,27 @@
+package main
+
+import "encoding/json"
+
+func main() {
+	// 将json字符串，反序列化成struct
+	test101()
+}
+func test101() {
+	// 将json字符串，反序列化成struct
+	str := `{"age":18,"name":"jack","address":"北京"}`
+	var p Person1
+	// 反序列化
+	err := json.Unmarshal([]byte(str), &p)
+	if err != nil {
+		println("反序列化失败")
+	}
+	println(p.Name, p.Age, p.Address)
+	// 分割线
+	println("-------------分割线--------------")
+}
+
+type Person1 struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Address string `json:"address"`
+}
