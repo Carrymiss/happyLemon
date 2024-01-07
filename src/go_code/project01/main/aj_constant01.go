cf53be1d47da9907ed0ea9618f3ba03ca3216f84
CarryMiss <1023638599@qq.com>
2024-01-07 14:45:18 +0800

    happyLemon 常量 00312

--- src/go_code/project01/main/aj_constant01.go
+++ src/go_code/project01/main/aj_constant01.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func main() {
+	// 常量
+	test122()
+}
+
+func test122() {
+	var num int
+	//常量声明的时候必须赋值 不能给默认值
+	const pi = 3.1415926
+	fmt.Println("num=", num, "pi=", pi)
+}
